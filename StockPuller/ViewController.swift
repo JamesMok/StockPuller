0355a5aeaf4b0ba9a4b83282003bbd759be62501
JamesMok <jmok@hotmail.com>
2018-06-26 10:21:30 -0400

    Initial Commit

---
+++ StockPuller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockPuller
+//
+//  Created by James Mok on 2018-06-26.
+//  Copyright © 2018 James Mok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
